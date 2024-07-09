1f9d3d156b8a8221de80b849d850525c1b5fd5a1
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2024-07-09 12:32:27 -0300

    feat: add tailwind css extended theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,18 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "var(--color-text)",
+        "text-light": "var(--color-text-light)",
+        background: "var(--color-background)",
+        primary: "var(--color-primary)",
+        "primary-dark": "var(--color-primary-dark)",
+        gold: "var(--color-gold)",
+        paper: "var(--color-paper)",
+        orange: "var(--color-orange)",
+      },
+    },
   },
   plugins: [],
 };
