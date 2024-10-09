232038ca54a9813416bfe4fd202290b4cb4ef96a
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2024-10-09 14:33:53 -0300

    feat: add Link to HorizontalCard component

--- src/components/card/HorizontalCard.tsx
+++ src/components/card/HorizontalCard.tsx
@@ -1,15 +1,22 @@
+import Link from "next/link";
 import { HiOutlineBookOpen } from "react-icons/hi";
 import { LuPencil } from "react-icons/lu";
 import { RxVideo } from "react-icons/rx";
 
 interface IHorizontalCardProps {
   text: string;
+  path: string;
 }
 
-export default function HorizontalCard({ text }: IHorizontalCardProps) {
+export default function HorizontalCard({ text, path }: IHorizontalCardProps) {
   return (
     <article className="flex justify-between items-center h-10 bg-primary-dark rounded-lg text-text-contrast">
-      <h1 className="ml-6 text-base font-semibold">{text}</h1>
+      <Link
+        href={path || ""}
+        className="w-full h-full flex items-center rounded-l-lg hover:text-primary hover:underline"
+      >
+        <h1 className="ml-6 text-base font-semibold">{text}</h1>
+      </Link>
       <div className="h-full flex items-center justify-center px-3 gap-x-3 bg-primary rounded-r-lg">
         <HiOutlineBookOpen className="size-6" />
         <RxVideo className="size-6" />
