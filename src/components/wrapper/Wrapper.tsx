6ef9003d05eea935b69921f44c4288b373ea5a06
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-01-19 12:26:41 -0300

    feat: conditionally render children in Wrapper based on pathname

--- src/components/wrapper/Wrapper.tsx
+++ src/components/wrapper/Wrapper.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { usePathname } from "next/navigation";
 import React, { useState } from "react";
 import Header from "../header/Header";
 import { SideMenu } from "../sideMenu/SideMenu";
@@ -10,16 +11,20 @@ interface IWrapperProps {
 export function Wrapper({ children }: IWrapperProps) {
   const [expanded, setExpanded] = useState(true);
 
+  const pathName = usePathname();
+
   const toggleExpanded = () => {
     setExpanded((curr) => !curr);
   };
 
-  return (
+  return pathName.includes("/login") ? (
+    children
+  ) : (
     <div className="flex">
       <SideMenu expanded={expanded} toggleExpanded={toggleExpanded} />
       <main
         className={`flex flex-1 flex-col flex-wrap justify-center mr-4 p-6 transition-all duration-200 delay-150 gap-y-6
-        ${expanded ? "md:ml-60" : "md:ml-16"}`}
+    ${expanded ? "md:ml-60" : "md:ml-16"}`}
       >
         <Header />
         {children}
