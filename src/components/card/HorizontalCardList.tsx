fb00d734f78e80279631ae8f6d8b35eb153e69a2
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-02-24 16:45:21 -0300

    feat: refactor HorizontalCardList to use fetchServer for topic retrieval and add error handling

--- src/components/card/HorizontalCardList.tsx
+++ src/components/card/HorizontalCardList.tsx
@@ -1,26 +1,47 @@
-import { Subject, Topic } from "@/types/subject";
+import { fetchServer } from "@/libs/fetchServer";
+import { Topic } from "@/types/subject";
 import HorizontalCard from "./HorizontalCard";
 
 interface HorizontalCardListProps {
   id: string;
 }
 
-async function getData(id: string) {
-  const res = await fetch(`http://localhost:5000/subjects/?id=${id}`, {
-    next: {
-      revalidate: 0,
-    },
-  });
+async function getTopics(id: string) {
+  try {
+    const res = await fetchServer(
+      `http://localhost:8080/api/v1/topics/subjectId/${id}`,
+      {
+        next: {
+          revalidate: 0,
+        },
+      }
+    );
 
-  const data: Subject[] = await res.json();
-
-  return data[0].topics;
+    return await res.json();
+  } catch (err) {
+    console.error("Erro ao buscar os tópicos: ", err);
+    return [];
+  }
 }
+
+// async function getData(id: string) {
+//   const res = await fetch(`http://localhost:5000/subjects/?id=${id}`, {
+//     next: {
+//       revalidate: 0,
+//     },
+//   });
+
+//   const data: Subject[] = await res.json();
+
+//   return data[0].topics;
+// }
 
 export default async function HorizontalCardList({
   id,
 }: HorizontalCardListProps) {
-  const topics = await getData(id);
+  const topics = await getTopics(id);
+
+  console.log(topics);
 
   return (
     <>
