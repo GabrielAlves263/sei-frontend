4aeaedf2721b549d12471f106fbc915f63a07d28
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-02-24 16:26:08 -0300

    feat: enhance CardList to fetch user-specific favorite subjects and handle errors

--- src/components/card/CardList.tsx
+++ src/components/card/CardList.tsx
@@ -1,28 +1,62 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import { fetchServer } from "@/libs/fetchServer";
 import { Subject } from "@/types/subject";
+import { getServerSession } from "next-auth";
 import { Card } from "./Card";
 
 interface CardListProps {
   onlyFavoriteds?: boolean;
 }
 
-async function getSubjects(onlyFavoriteds: boolean) {
-  const url = onlyFavoriteds
-    ? "http://localhost:5000/subjects/?favorited=true"
-    : "http://localhost:5000/subjects";
+async function getSubjects(onlyFavoriteds: boolean, userId?: string) {
+  const url =
+    onlyFavoriteds && userId
+      ? `http://localhost:8080/api/v1/subjects/idUser/${userId}`
+      : "http://localhost:8080/api/v1/subjects";
 
-  const res = await fetch(url, {
-    next: {
-      revalidate: 0,
-    },
-  });
+  try {
+    const res = await fetchServer(url, {
+      next: {
+        revalidate: 0,
+      },
+    });
 
-  return res.json();
+    return await res.json();
+  } catch (err) {
+    console.error("Erro ao buscar disciplinas:", err);
+    return [];
+  }
 }
+
+// async function getSubjects(onlyFavoriteds: boolean) {
+//   const url = onlyFavoriteds
+//     ? "http://localhost:5000/subjects/?favorited=true"
+//     : "http://localhost:5000/subjects";
+
+//   const res = await fetch(url, {
+//     next: {
+//       revalidate: 0,
+//     },
+//   });
+
+//   return res.json();
+// }
 
 export default async function CardList({
   onlyFavoriteds = false,
 }: CardListProps) {
-  const subjects = await getSubjects(onlyFavoriteds);
+  const session = await getServerSession(authOptions);
+  const userId = session?.user?.id;
+
+  const userSubjects = await getSubjects(true, userId);
+  const favoriteIds = new Set(
+    userSubjects.map((subject: Subject) => subject.id)
+  );
+
+  const subjects = onlyFavoriteds ? userSubjects : await getSubjects(false);
+
+  console.log("Subjects:");
+  console.log(userSubjects);
 
   return (
     <>
@@ -31,7 +65,7 @@ export default async function CardList({
           key={subject.id}
           text={subject.name}
           path={`/disciplina/${subject.id}`}
-          favorited={subject.favorited}
+          favorited={favoriteIds.has(subject.id)}
         />
       ))}
     </>
