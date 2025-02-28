9bb1644ac0db85a2071adccbe69c4b66f7565e84
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-02-28 10:32:01 -0300

    feat: refactor question fetching in PageQuestoes to use fetchServer and improve error handling

--- src/app/disciplina/[id]/[topic_name]/questoes/page.tsx
+++ src/app/disciplina/[id]/[topic_name]/questoes/page.tsx
@@ -1,43 +1,57 @@
 import NotFound from "@/components/notFound/NotFound";
-import { Subject, Topic } from "@/types/subject";
+import { fetchServer } from "@/libs/fetchServer";
 
 interface Props {
   params: { id: string; topic_name: string };
 }
 
-async function getData(id: string, topic_name: string) {
-  const res = await fetch(`http://localhost:5000/subjects/?id=${id}`, {
-    next: {
-      revalidate: 0,
-    },
-  });
+async function getQuestions(id: string, topic_name: string) {
+  try {
+    const res = await fetchServer(
+      `http://localhost:8080/api/v1/topics/subjectId/${id}`
+    );
 
-  const data: Subject[] = await res.json();
+    const topics = await res.json();
 
-  return data[0].topics.filter(
-    (topic) => topic.name === topic_name.replaceAll("-", " ")
-  );
+    const topic = topics.filter(
+      (topic: any) => topic.name === topic_name.replaceAll("-", " ")
+    );
+
+    // Ainda não retorna as questões. Implementação futura
+    return topic[0];
+  } catch (err) {
+    console.error("Erro ao carregar as questões: ", err);
+    return null;
+  }
 }
+
+// async function getData(id: string, topic_name: string) {
+//   const res = await fetch(`http://localhost:5000/subjects/?id=${id}`, {
+//     next: {
+//       revalidate: 0,
+//     },
+//   });
+
+//   const data: Subject[] = await res.json();
+
+//   return data[0].topics.filter(
+//     (topic) => topic.name === topic_name.replaceAll("-", " ")
+//   );
+// }
 
 export default async function PageQuestoes({ params }: Props) {
   params.topic_name = decodeURI(params.topic_name);
-  const topic: Topic[] = await getData(params.id, params.topic_name);
-  const questions = topic[0].questions ?? [];
+  const topic = await getQuestions(params.id, params.topic_name);
 
   return (
     <>
-      {questions.length > 0 ? (
-        "Questões"
-      ) : (
-        <NotFound>
-          <p>
-            Nenhuma questão sobre{" "}
-            <strong className="text-orange">{topic[0].name}</strong> foi
-            encontrada!
-          </p>
-          <p>Novas questões serão adicionadas em breve.</p>
-        </NotFound>
-      )}
+      <NotFound>
+        <p>
+          Nenhuma questão sobre{" "}
+          <strong className="text-orange">{topic?.name}</strong> foi encontrada!
+        </p>
+        <p>Novas questões serão adicionadas em breve.</p>
+      </NotFound>
     </>
   );
 }
