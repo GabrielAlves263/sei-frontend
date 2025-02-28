ed3b928dcf85f5f562d664d061c6db4eeb2c539c
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-02-28 11:21:40 -0300

    feat: refactor subject fetching in PageSimulados to use fetchServer and improve error handling

--- src/app/disciplina/[id]/simulados/page.tsx
+++ src/app/disciplina/[id]/simulados/page.tsx
@@ -3,7 +3,7 @@ import LabelList from "@/components/label/LabelList";
 import Navigator from "@/components/navigator/Navigator";
 import OptionsBar from "@/components/optionsMenu/OptionsMenu";
 import { getSubjectOptions } from "@/constants/options";
-import { Subject } from "@/types/subject";
+import { fetchServer } from "@/libs/fetchServer";
 import { Metadata } from "next";
 
 interface Props {
@@ -11,51 +11,56 @@ interface Props {
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const res = await fetch(`http://localhost:5000/subjects/?id=${params.id}`);
+  const subject = await getSubject(params.id);
 
-  if (!res.ok) {
-    return {
-      title: "Disciplina não encontrada",
-    };
-  }
-
-  const data: Subject[] = await res.json();
-
-  if (data.length === 0) {
+  if (!subject) {
     return {
       title: "Disciplina não encontrada",
     };
   }
 
   return {
-    title: data[0].name,
+    title: subject.name,
   };
 }
 
-async function getData(id: string) {
-  const res = await fetch(`http://localhost:5000/subjects/?id=${id}`);
+async function getSubject(id: string) {
+  try {
+    const res = await fetchServer(
+      `http://localhost:8080/api/v1/subjects/${id}`
+    );
 
-  if (!res.ok) {
-    return {
-      name: "Erro: Disciplina não encontrada",
-    };
+    return await res.json();
+  } catch (err) {
+    console.error(err);
+    return null;
   }
-
-  const data: Subject[] = await res.json();
-
-  if (data.length === 0) {
-    return {
-      name: "Erro: Disciplina não encontrada",
-    };
-  }
-
-  return {
-    name: data[0].name,
-  };
 }
+
+// async function getData(id: string) {
+//   const res = await fetch(`http://localhost:5000/subjects/?id=${id}`);
+
+//   if (!res.ok) {
+//     return {
+//       name: "Erro: Disciplina não encontrada",
+//     };
+//   }
+
+//   const data: Subject[] = await res.json();
+
+//   if (data.length === 0) {
+//     return {
+//       name: "Erro: Disciplina não encontrada",
+//     };
+//   }
+
+//   return {
+//     name: data[0].name,
+//   };
+// }
 
 export default async function PageSimulados({ params }: Props) {
-  const subject = await getData(params.id);
+  const subject = await getSubject(params.id);
   const paths = [
     { url: `/disciplina/${params.id}`, text: subject.name },
     { url: `/disciplina/${params.id}/simulados`, text: "Simulados" },
