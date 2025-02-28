abe4b0c269e212e5d5ef5879af19438016b358c6
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-02-28 12:10:25 -0300

    fix: simplify Link component structure in Label for improved readability

--- src/components/label/Label.tsx
+++ src/components/label/Label.tsx
@@ -8,15 +8,10 @@ interface LabelProps {
 
 export default function Label({ text, path, isAP = false }: LabelProps) {
   return isAP ? (
-    <Link href={path || ""} legacyBehavior>
-      <a
-        className="w-full h-full flex items-center justify-center"
-        target="_blank"
-      >
-        <div className="w-64 h-10 flex items-center px-3 py-2 justify-center bg-primary-dark text-text-contrast font-semibold rounded-lg hover:bg-primary hover:underline hover:text-paper cursor-pointer">
-          {text}
-        </div>
-      </a>
+    <Link href={path || ""} target="_blank">
+      <div className="w-64 h-10 flex items-center px-3 py-2 justify-center bg-primary-dark text-text-contrast font-semibold rounded-lg hover:bg-primary hover:underline hover:text-paper cursor-pointer">
+        {text}
+      </div>
     </Link>
   ) : (
     <Link className="w-fit rounded-lg" href={path}>
