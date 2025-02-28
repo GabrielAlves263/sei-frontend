e506b88b92fa347e48237e90224148a5df7b3c75
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-02-28 10:19:41 -0300

    feat: refactor data fetching in PageResume to use fetchServer and improve error handling

--- src/app/disciplina/[id]/[topic_name]/page.tsx
+++ src/app/disciplina/[id]/[topic_name]/page.tsx
@@ -1,4 +1,4 @@
-import { Subject, Topic } from "@/types/subject";
+import { fetchServer } from "@/libs/fetchServer";
 import { Metadata } from "next";
 
 interface Props {
@@ -13,33 +13,57 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-async function getData(id: string, topic_name: string) {
-  const res = await fetch(`http://localhost:5000/subjects/?id=${id}`, {
-    next: {
-      revalidate: 0,
-    },
-  });
+async function getResume(id: string, topic_name: string) {
+  try {
+    const res = await fetchServer(
+      `http://localhost:8080/api/v1/topics/subjectId/${id}`
+    );
 
-  const data: Subject[] = await res.json();
+    const topics = await res.json();
 
-  return data[0].topics.filter(
-    (topic) => topic.name === topic_name.replaceAll("-", " ")
-  );
+    const topic = topics.filter(
+      (topic: any) => topic.name === topic_name.replaceAll("-", " ")
+    );
+
+    return topic[0].resume;
+  } catch (err) {
+    console.error("Erro ao carregar o resumo: ", err);
+    return null;
+  }
 }
+
+// async function getData(id: string, topic_name: string) {
+//   const res = await fetch(`http://localhost:5000/subjects/?id=${id}`, {
+//     next: {
+//       revalidate: 0,
+//     },
+//   });
+
+//   const data: Subject[] = await res.json();
+
+//   return data[0].topics.filter(
+//     (topic) => topic.name === topic_name.replaceAll("-", " ")
+//   );
+// }
 
 export default async function PageResume({ params }: Props) {
   params.topic_name = decodeURI(params.topic_name);
 
-  const topic: Topic[] = await getData(params.id, params.topic_name);
+  const resume = await getResume(params.id, params.topic_name);
+
+  console.log("Topic:");
+  console.log(resume);
 
   return (
-    <div
-      key={topic[0].id}
-      dangerouslySetInnerHTML={
-        topic[0].resume
-          ? { __html: topic[0].resume }
-          : { __html: "Resumo não encontrado!" }
-      }
-    />
+    <>
+      {resume ? (
+        <div>
+          <h4 className="font-semibold">{resume.title}</h4>
+          <p className="text-justify indent-14">{resume.text}</p>
+        </div>
+      ) : (
+        <p>Resumo não encontrado!</p>
+      )}
+    </>
   );
 }
